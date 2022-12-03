14ddca37b5f2e4f0e666b2e976aa2fccb599bba9
Ian Jones <ijones@splashfinancial.com>
2022-12-03 12:58:56 -0700

    mapcat

--- src/day3.clj
+++ src/day3.clj
@@ -8,12 +8,10 @@
      (util/parse-input)
      (map #(split-at (/ (count %) 2) (into [] %)))
      (map (fn [cols] (map #(into #{} %) cols)))
-     (map (fn [[s1 s2]] (into [] (set/intersection s1 s2))))
-     flatten
-     (map (fn [s]
-            (if (Character/isUpperCase s)
-              (- (int s) 38)
-              (- (int s) 96))))
+     (mapcat (fn [[s1 s2]] (into [] (set/intersection s1 s2))))
+     (map #(if (Character/isUpperCase %)
+             (- (int %) 38)
+             (- (int %) 96)))
      (apply +))
 
 
@@ -26,10 +24,9 @@
                 seq
                 (into #{})))
      (partition 3)
-     (map #(->> %
-                (apply set/intersection)
-                (into [])))
-     flatten
+     (mapcat #(->> %
+                   (apply set/intersection)
+                   (into [])))
      (map #(if (Character/isUpperCase %)
              (- (int %) 38)
              (- (int %) 96)))
